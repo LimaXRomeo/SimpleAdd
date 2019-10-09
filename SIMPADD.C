83506ba6150d6d7b3dad4913a9cbe5777b716ca8
LimaXRomeo <lixinrui0001@hotmail.com>
2019-10-09 16:32:54 +0800

    1.6 Update 10/9/2019

--- SIMPADD.C
+++ SIMPADD.C
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int a,b,c,d,e,i=1,k=1,t,o=0;
+    int c,i=1;
     printf("--------- Simple+ ---------\n");
     printf("       By LimaXRomeo       \n\n");
     printf("Type two demical numbers and I'll add them together.\n");
@@ -9,19 +9,28 @@ int main()
     printf("\nPress ENTER when you finish every number.\n\n ");
     while (i==1)
     {
-	scanf("%d",&a);
+	long long a,b,d,e,k=1,o=0,t;
+	scanf("%ld",&a);
 	printf("+");
-	scanf("%d",&b);
+	scanf("%ld",&b);
 	t=a+b;
-	e=a+b;
-	while(e)
+	if(t<1024)
 	{
-	    d=e%2;
-	    e=e/2;
-	    o=o+d*k;
-	    k=k*10;
+	    e=a+b;
+	    while(e!=0)
+	    {
+		d=e%2;
+		printf("d=%ld e=%ld o=%ld k=%ld",d,e,o,k);
+		e=e/2;
+		o=o+d*k;
+		k=k*10;
+	    }
+	    printf("\n----------\n=%ld\n\nBin:%ld\nOct: %#lo\nHex: %#lx\n\n",t,o,t,t);
 	}
-	printf("\n----------\n=%d\n\nOct: %#o\nHex: %#x\n\n",t,t,t);
+	else
+	{
+	    printf("\n----------\n=%ld\n\nBin: N/A\nOct: %#lo\nHex: %#lx\n\n",t,t,t);
+	}
 	printf("Press 0 and then ENTER to exit.\n");
 	printf("Type 1 then ENTER to continue.\n");
     loop:scanf("%d",&c);
