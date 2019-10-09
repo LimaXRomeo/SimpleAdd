d6ac25a4d6fd1a9d0776475bfbea9bb270ce6b18
LimaXRomeo <lixinrui0001@hotmail.com>
2019-10-09 19:48:28 +0800

    1.6 Update 10/8/2019

--- Simple+.c
+++ Simple+.c
@@ -3,17 +3,18 @@
 int main()
 {
 	system("color 3F");
-	int a, b, c, t, i = 1;
+	long a, b, t;
+	int i = 1;
 	while (i == 1)
 	{
 		printf("--------- Simple+ ---------\n       By LimaXRomeo       \n\nType two decimal numbers and I'll add them together.\nBinary, octal and hexadecimal numbers of the result will also be shown.\nPress ENTER when you finish every number.\n\n ");
-		scanf_s("%d", &a);
+		scanf_s("%ld", &a);
 		printf("+");
-		scanf_s("%d", &b);
+		scanf_s("%ld", &b);
 		t = a + b;
-		void dectobin(int n);
+		if (t < 1024)
 		{
-			int d, e, o = 0, k = 1;
+		    long d, e, o = 0, k = 1;
 			e = a + b;
 			while (e)
 			{
@@ -22,21 +23,27 @@ int main()
 				o = o + d * k;
 				k = k * 10;
 			}
-			printf("------------\n=%d\n\nBin: %d\nOct: %#o\nHex: %#x\n\nType 0 and ENTER to exit.\nType 1 and ENTER to continue.\n", t, o, t, t);
-		loop:scanf_s("%d", &c);
-			if (c == 0)
-			{
-				return 0;
-			}
-			else if (c == 1)
-			{
-				system("cls");
-			}
-			else
-			{
-				printf("Invalid!\n");
-				goto loop;
-			}
+			printf("------------\n=%ld\n\nBin: %ld\nOct: %#lo\nHex: %#lx", t, o, t, t);
+		}
+		else
+		{
+			printf("----------\n=%ld\n\nBin: N/A\nOct: %#lo\nHex: %#lx", t, t, t);
+		}
+		printf("\n\nType 0 and ENTER to exit.\nType 1 and ENTER to continue.\n");
+		int c;
+	loop:scanf_s("%d", &c);
+		if (c == 0)
+		{
+			return 0;
+		}
+		else if (c == 1)
+		{
+			system("cls");
+		}
+		else
+		{
+			printf("Invalid!\n");
+			goto loop;
 		}
 	}
 }
