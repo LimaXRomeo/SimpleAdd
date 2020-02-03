a659c9eddf45e343f767d41fec213444e66acd64
李昕睿 <lixinrui0001@hotmail.com>
2020-02-03 20:53:50 +0800

    2.0 For Win32
    2.0 Update for Win32
    -Binary output is now available for results smaller than 524288.

--- Simple+.c
+++ Simple+.c
@@ -3,18 +3,18 @@
 int main()
 {
 	system("color 3F");
-	long a, b, t;
+	long long int a, b, t;
 	int i = 1;
 	while (i == 1)
 	{
 		printf("--------- Simple+ ---------\n       By LimaXRomeo       \n\nType two decimal numbers and I'll add them together.\nBinary, octal and hexadecimal numbers of the result will also be shown.\nPress ENTER when you finish every number.\n\n ");
-		scanf_s("%ld", &a);
+		scanf_s("%lld", &a);
 		printf("+");
-		scanf_s("%ld", &b);
+		scanf_s("%lld", &b);
 		t = a + b;
-		if (t < 1024)
+		if (t < 524288)
 		{
-		    long d, e, o = 0, k = 1;
+		    long long int d, e, o = 0, k = 1;
 			e = a + b;
 			while (e)
 			{
@@ -23,11 +23,11 @@ int main()
 				o = o + d * k;
 				k = k * 10;
 			}
-			printf("------------\n=%ld\n\nBin: %ld\nOct: %#lo\nHex: %#lx", t, o, t, t);
+			printf("------------\n=%lld\n\nBin: %lld\nOct: %#llo\nHex: %#llx", t, o, t, t);
 		}
 		else
 		{
-			printf("----------\n=%ld\n\nBin: N/A\nOct: %#lo\nHex: %#lx", t, t, t);
+			printf("----------\n=%lld\n\nBin: N/A\nOct: %#llo\nHex: %#llx", t, t, t);
 		}
 		printf("\n\nType 0 and ENTER to exit.\nType 1 and ENTER to continue.\n");
 		int c;
