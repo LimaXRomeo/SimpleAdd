725669198934b8161d8ed53c9163c25267cc90a9
李昕睿 <lixinrui0001@hotmail.com>
2020-03-24 15:18:38 +0800

    2.2 Update
    Maybe final?

--- SIMPADD.C
+++ SIMPADD.C
@@ -1,52 +1,32 @@
 #include <stdio.h>
-int main()
+void main()
 {
-    int c,i=1;
-    printf("--------- Simple+ ---------\n");
-    printf("       By LimaXRomeo       \n\n");
-    printf("Type two demical numbers and I'll add them together.\n");
-    printf("Octal and hexadecimal numbers of the result will also be shown.");
-    printf("\nPress ENTER when you finish every number.\n\n ");
-    while (i==1)
-    {
-	long long a,b,d,e,k=1,o=0,t;
-	scanf("%ld",&a);
+    unsigned long a,b,d,e,k=1,o=0,t;
+    printf("----------- Simple+ -----------\n");
+    printf("       By LimaXRomeo       \n");
+    do{
+	printf("Please enter your data:\n ");
+	k=1,o=0;
+	scanf("%lu",&a);
 	printf("+");
-	scanf("%ld",&b);
+	scanf("%lu",&b);
 	t=a+b;
 	if(t<1024)
 	{
-	    e=a+b;
-	    while(e!=0)
+	    e=t;
+	    while(e)
 	    {
 		d=e%2;
-		printf("d=%ld e=%ld o=%ld k=%ld",d,e,o,k);
 		e=e/2;
 		o=o+d*k;
 		k=k*10;
 	    }
-	    printf("\n----------\n=%ld\n\nBin:%ld\nOct: %#lo\nHex: %#lx\n\n",t,o,t,t);
+	    printf("\n----------\n=%lu\n\nBin: %lu\nOct: %#lo\nHex: %#lx\n\n",t,o,t,t);
 	}
 	else
 	{
-	    printf("\n----------\n=%ld\n\nBin: N/A\nOct: %#lo\nHex: %#lx\n\n",t,t,t);
+	    printf("\n----------\n=%lu\n\nBin: N/A\nOct: %#lo\nHex: %#lx\n\n",t,t,t);
 	}
-	printf("Press 0 and then ENTER to exit.\n");
-	printf("Type 1 then ENTER to continue.\n");
-    loop:scanf("%d",&c);
-	if (c==0)
-	{
-	    return 0;
-	}
-	else if (c==1)
-	{
-	    printf("\nPlease continue.\n ");
-	}
-	else
-	{
-	    printf("Invalid!\n");
-	    goto loop;
-	}
-    }
-return 1;
+	printf("Press 1 to continue.\nPress ENTER to exit.\n\n");
+    }while(getch()=='1');
 }
